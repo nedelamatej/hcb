e0edc9f1d17a65bc0ca3eee48523baa410bc8369
Matěj Neděla <nedela.matej@gmail.com>
2021-10-16 18:18:58 +0200

    Third commit

--- src/Map1.svelte
+++ src/Map1.svelte
@@ -5,6 +5,7 @@
 
   let bins = BINS.filter((bin) => bin.druh == 'plasty');
 
+  let map;
   let circlesCenter;
   let circlesRadius;
 
@@ -44,14 +45,56 @@
     else if (type === 'textil') return 400;
     else if (type === 'tuky/oleje') return 400;
   }
+
+  $: if (value) {
+    circlesCenter.forEach((circle) => {
+      circle.setMap(null);
+    });
+    circlesRadius.forEach((circle) => {
+      circle.setMap(null);
+    });
+
+    bins = BINS.filter((bin) => bin.druh === value);
+
+    circlesCenter = bins.map((bin) => {
+      return new google.maps.Circle({
+        strokeColor: '#FFFFFF',
+        strokeOpacity: 0,
+        strokeWeight: 0,
+        fillColor: getFillColor(bin.druh),
+        fillOpacity: 1,
+        center: {
+          lat: Number(bin.x.replace(',', '.')),
+          lng: Number(bin.y.replace(',', '.')),
+        },
+        radius: 20,
+      });
+    });
+
+    circlesRadius = bins.map((bin) => {
+      return new google.maps.Circle({
+        strokeColor: '#FFFFFF',
+        strokeOpacity: 0,
+        strokeWeight: 0,
+        fillColor: getFillColor(bin.druh),
+        fillOpacity: getFillOpacity(bin),
+        center: {
+          lat: Number(bin.x.replace(',', '.')),
+          lng: Number(bin.y.replace(',', '.')),
+        },
+        radius: getRadius(bin.druh),
+      });
+    });
+
+    circlesCenter.forEach((circle) => {
+      circle.setMap(map);
+    });
+    circlesRadius.forEach((circle) => {
+      circle.setMap(map);
+    });
+  }
    
-  onMount(async () => { createMap() });
-
-  // $: {
-  //   createMap(value);
-  // }
-
-  function createMap() {
+  onMount(async () => {
     map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 48.9744689, lng: 14.4743419 },
       zoom: 14,
@@ -95,7 +138,7 @@
     circlesRadius.forEach((circle) => {
       circle.setMap(map);
     });
-  }
+  });
 </script>
 
 <div id='map'></div>
